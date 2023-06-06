e72b09a607a5037aac10bc51886d9fa82fc867fe
roddg <roddg@mail.ru>
2023-06-06 23:02:57 +0900

    hw 6 solved

--- dz6.sql
+++ dz6.sql
@@ -1,10 +1,46 @@
 -- Создайте функцию, которая принимает кол-во сек и формат их в кол-во дней часов. Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
 
+DELIMITER //
+CREATE PROCEDURE times(seconds INT)
+BEGIN
+	DECLARE days, hours, minutes INT DEFAULT 0;
+    DECLARE sec_per_minutes INT DEFAULT 60;
+    DECLARE min_per_hours INT DEFAULT 60;
+    DECLARE hours_per_days INT DEFAULT 24;
+    
+    SET minutes = seconds DIV sec_per_minutes;
+    SET seconds = seconds % sec_per_minutes;
+    SET hours = minutes DIV min_per_hours;
+    SET minutes = minutes % min_per_hours;
+    SET days = hours DIV hours_per_days;
+    SET hours = hours % hours_per_days;
 
+    SELECT concat(days, ' days ', hours, ' hours ', minutes, ' minutes ', seconds, ' seconds ') AS result;
+END //
+DELIMITER ;
+
+CALL times(123456);
 
 -- Выведите только четные числа от 1 до 10. Пример: 2,4,6,8,10 
---Данная промежуточная аттестация оценивается по системе "зачет" / "не зачет" "Зачет" ставится, 
--- если слушатель успешно выполнил 1 или 2 задачи "Незачет" ставится, если слушатель успешно выполнил 0 задач 
--- Критерии оценивания: 1 - слушатель верно создал функцию, которая принимает кол-во сек и формат их в кол-во дней часов. 
--- 2 - слушатель выведили только четные числа от 1 до 10.
+
+DELIMITER //
+CREATE PROCEDURE even_numbers(`start` INT, `end` INT)
+BEGIN
+	DECLARE i INT DEFAULT `start`;
+    DECLARE result TEXT DEFAULT NULL;
+    WHILE  i<=`end` DO
+        IF i%2 = 0 THEN
+			IF result IS NULL THEN
+				SET result = concat(i);
+			ELSE
+				SET result = concat(result, ',', i);
+			END IF;
+		END IF;
+        SET i = i + 1;
+    END WHILE;
+	SELECT result;
+END //
+DELIMITER ;
+
+CALL even_numbers(1, 10);
 
